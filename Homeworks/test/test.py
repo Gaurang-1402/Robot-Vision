e8f7b13bbfbc899cae06cd6c8bdae0300e5ba481
Gaurang Ruparelia <gr2159@nyu.edu>
2023-02-04 23:25:12 -0500

    make the HW work once, TODO add the fancy modifications

--- Homeworks/test/test.py
+++ Homeworks/test/test.py
@@ -53,30 +53,30 @@ criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
 
 # # Train the model
-# for epoch in range(10):
-#     for i, (images, labels) in enumerate(train_loader):
-#         outputs = model(images)
-#         loss = criterion(outputs, labels)
+for epoch in range(10):
+    for i, (images, labels) in enumerate(train_loader):
+        outputs = model(images)
+        loss = criterion(outputs, labels)
         
-#         optimizer.zero_grad()
-#         loss.backward()
-#         optimizer.step()
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
         
-#         if (i+1) % 100 == 0:
-#             print (f'Epoch [{epoch+1}/10], Step [{i+1}/600], Loss: {loss.item():.4f}')
-#     torch.save(model, './model1_'+str(epoch)+'.pt')
+        if (i+1) % 100 == 0:
+            print (f'Epoch [{epoch+1}/10], Step [{i+1}/600], Loss: {loss.item():.4f}')
+    torch.save(model, './model_'+str(epoch)+'.pt')
     
-# # Test the model
-# with torch.no_grad():
-#     correct = 0
-#     total = 0
-#     for images, labels in test_loader:
-#         outputs = model(images)
-#         _, predicted = torch.max(outputs.data, 1)
-#         total += labels.size(0)
-#         correct += (predicted == labels).sum().item()
+# Test the model
+with torch.no_grad():
+    correct = 0
+    total = 0
+    for images, labels in test_loader:
+        outputs = model(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
 
-#     print(f'Accuracy of the network on the 10000 test images: {100 * correct / total}%')
+    print(f'Accuracy of the network on the 10000 test images: {100 * correct / total}%')
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
@@ -97,14 +97,20 @@ test_model = test_model.to(device =device)
 # 8	Bag
 # 9	Ankle boot
 
+
+image = cv2.imread('./test_images/coat.jpg', cv2.IMREAD_GRAYSCALE)
+
 # Load test images from local folder
 test_images_folder = './test_images'
 test_images = []
 test_labels = []
 
+test_images.append(image)
+test_labels.append(4)
+
 for image_name in os.listdir(test_images_folder):
     image = cv2.imread(os.path.join(test_images_folder, image_name), cv2.IMREAD_GRAYSCALE)
-    label = image_name.split('.')[0] # assuming that the label is the first part of the file name, separated by '_'
+    label = image_name.split('_')[0] # assuming that the label is the first part of the file name, separated by '_'
     test_images.append(image)
     test_labels.append(label)
 
